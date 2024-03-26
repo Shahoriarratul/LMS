a98840570ee0b807798d9f518c03f8ac5192d8b5
Ratul <shahoriar.ratul@gmail.com>
2024-03-26 13:07:32 +0600

    build(deps): update page.tsx, package-lock.json, package.json and schema.prisma

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,5 +1,59 @@
-function CourseIdPage({ params }: { params: { courseId: String } }) {
-  return <div>Course id: {params.courseId}</div>;
-}
+import { IconBadge } from "@/components/icon-badge";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { LayoutDashboard } from "lucide-react";
+import { redirect } from "next/navigation";
+import TitleForm from "./_components/title-form";
+import DescriptionForm from "./_components/description-form";
+
+const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
+  const { userId } = auth();
+  if (!userId) {
+    return redirect("/");
+  }
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+  });
+
+  if (!course) {
+    return redirect("/");
+  }
+
+  const requiredField = [
+    course.title,
+    course.description,
+    course.price,
+    course.catagoryId,
+    course.imageUrl,
+  ];
+
+  const totalFields = requiredField.length;
+  const completedFields = requiredField.filter(Boolean).length;
+  const compleationText = `(${completedFields}/${totalFields})`;
+  return (
+    <div className="p-6">
+      <div className="flex items-center justify-center">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-medium"> Course setup</h1>
+          <span className="text-sm text-slate-700">
+            complete all fields {compleationText}
+          </span>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div>
+          <div className="flex items-center gap-x-2">
+            <IconBadge icon={LayoutDashboard} />
+            <h2 className="text-xl">Customize your course</h2>
+          </div>
+          <TitleForm initialData={course} courseId={course.id} />
+          <DescriptionForm initialData={course} courseId={course.id} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default CourseIdPage;
