59339994618e175f290e8645ac3f898b4fcbd1c2
Ratul <shahoriar.ratul@gmail.com>
2024-05-26 01:17:01 +0600

    update layout.tsx and navbar-routes.tsx

--- app/(course)/courses/[courseId]/layout.tsx
+++ app/(course)/courses/[courseId]/layout.tsx
@@ -3,6 +3,7 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import { CourseSidebar } from "./_components/course-sidebar";
+import CourseNavbar from "./_components/course-navbar";
 
 const CourseLayout = async ({
   children,
@@ -40,13 +41,16 @@ const CourseLayout = async ({
   if (!course) {
     return redirect("/");
   }
-  const progresscount = await getProgress(userId, course.id);
+  const progressCount = await getProgress(userId, course.id);
   return (
     <div className="h-full">
-      <div className="hidden md:flex h-full w-80 flex-col fixed inset-y-0 z-50">
-        <CourseSidebar course={course} progressCount={progresscount} />
+      <div className="h-[80px] md:pl-80 fixed inset-y-0 w-full z-50">
+        <CourseNavbar course={course} progressCount={progressCount} />
       </div>
-      <main className="md:pl-80 h-full">{children}</main>
+      <div className="hidden md:flex h-full w-80 flex-col fixed inset-y-0 z-50">
+        <CourseSidebar course={course} progressCount={progressCount} />
+      </div>
+      <main className="md:pl-80 pt-[80px]  h-full">{children}</main>
     </div>
   );
 };
