46d7f0e1b276f6091d28706d4b098aadde04d19c
Ratul <shahoriar.ratul@gmail.com>
2024-06-26 20:13:50 +0600

    update 6 files and delete 1 file

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,5 +1,48 @@
-function ChaterIdPage() {
-  return <div>ChapterId</div>;
-}
+import { getChapter } from "@/actions/get-chapters";
+import { Banner } from "@/components/banner";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const ChaterIdPage = async ({
+  params,
+}: {
+  params: { courseId: string; chapterId: string };
+}) => {
+  const { userId } = auth();
+  if (!userId) {
+    return redirect("/");
+  }
+  const {
+    chapter,
+    course,
+    muxData,
+    attachments,
+    nextChapter,
+    userProgress,
+    purchase,
+  } = await getChapter({
+    userId,
+    chapterId: params.chapterId,
+    courseId: params.courseId,
+  });
+  if (!chapter || !course) {
+    return redirect("/");
+  }
+  const isLocked = !chapter.isFree && !purchase;
+  const compleateOnEnd = !!purchase && !userProgress?.isCompleted;
+  return (
+    <div>
+      {userProgress?.isCompleted && (
+        <Banner variant="success" label="You already completed this chapter." />
+      )}
+      {isLocked && (
+        <Banner
+          variant="warning"
+          label="You need to purchase this course to watch this chapter."
+        />
+      )}
+    </div>
+  );
+};
 
 export default ChaterIdPage;
